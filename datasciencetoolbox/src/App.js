7bf3b41acbf0f2077de45147363ae615577494c0
Neel Trivedi <neeltrivedi@Neels-MacBook-Pro.local>
2020-11-22 19:32:51 -0500

    changed gui to reflect last commit

--- datasciencetoolbox/src/App.js
+++ datasciencetoolbox/src/App.js
@@ -14,8 +14,9 @@ function App() {
         <li><a href="https://www.pdesas.org/Profile/Account/Login">IBM SAS</a></li>
         <li><a href="http://localhost:9000">SonarQube/Scanner</a></li>
         <li><a href="http://localhost:12345">Markdown</a></li>
+        <li><a href="http://localhost:9901/?password=git">Git</a></li>
         <li>Tensorflow: run "docker exec -it datasciencetoolbox_tensorflowapp_1 bash" in terminal</li>
-        <li>Git: run "docker exec -it datasciencetoolbox_gitapp_1 /bin/sh" in terminal</li>
+        {/* <li>Git: run "docker exec -it datasciencetoolbox_gitapp_1 /bin/sh" in terminal</li> */}
         <li>Tableau run "docker exec -it datasciencetoolbox_tableauapp_1 bash" in terminal</li>
         <li>Hadoop: run "docker exec -it datasciencetoolbox_hadoopapp_1 bash" in terminal</li>
         <li>Spark: run "docker exec -it datasciencetoolbox_sparkapp_1 bash" in terminal</li>
